1fc585f877ed0f1123daaba49bcb8053464a1b65
CeciliaLucena <maria.cecilia.gouveia.lucena@ccc.ufcg.edu.br>
2024-09-17 12:10:19 -0300

    parcialmente finalizada

--- src/main/java/InsereFinalFilaPrioridade.java
+++ src/main/java/InsereFinalFilaPrioridade.java
@@ -9,12 +9,25 @@ public class InsereFinalFilaPrioridade implements FilaPrioridade {
 	}
 	
 	// criar um Pair e adicionar no fim da fila
-	public void add(String elemento, int prioridade) {}
+	public void add(String elemento, int prioridade) {
+		Pair pair = new Pair(elemento, prioridade);
+		this.fila.add(pair);
+	}
 
 
 	// buscar pelo elemento de maior prioridade na fila.
 	public String removeNext() {
-		return "";
+		if(!fila.isEmpty()){
+			Pair maiorPriori = this.fila.get(0);
+			for(int i = 1; i < fila.size(); i++) {
+				if(fila.get(i).getPrioridade() > maiorPriori.getPrioridade()) {
+					maiorPriori = fila.get(i);
+				}
+			}
+
+			return maiorPriori.getElemento();
+		}
+		return "Fila vazia.";
 	}
 
 }
--- src/main/java/InsereOrdenadoFilaPrioridade.java
+++ src/main/java/InsereOrdenadoFilaPrioridade.java
@@ -9,9 +9,30 @@ public class InsereOrdenadoFilaPrioridade implements FilaPrioridade {
 		this.last = -1;
 		this.head = -1;
 	}
+
+	public boolean isEmpty() {
+		return this.head == -1 && this.last == -1;
+	}
+
+	public boolean isFull() {
+		return (this.last + 1) % this.fila.length == this.head;
+	}
 	
 	// criar um Pair e inserir de forma ordenada decrescente no array.
-	public void add(String elemento, int prioridade) {}
+	public void add(String elemento, int prioridade) {
+		Pair pair = new Pair(elemento, prioridade);
+		if(!isFull()) {
+			last++;
+		}
+		if(isEmpty()) {
+			head = 0;
+		}
+		if(isFull()) {
+			this.head = (this.head + 1) % this.fila.length;
+		}
+		this.last = (this.last + 1) % this.fila.length;
+		this.fila[last] = pair;
+	}
 
 
 	// remover e retornar o primeiro elemento do array, que é o de maior prioridade. lembrar manipular head e tail
